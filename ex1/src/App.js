7d722cbf91715c90a198253df139587a3f8619a4
Nguyen1397 <knguyen4582@gmail.com>
2023-11-27 11:32:35 +0700

    Create App.js

---
+++ ex1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PricingPage from './component/PricingPage';
+
+export default function MyApp() {
+  return (
+    <div className='App'>
+      {/* {<ShoppingList />} */}
+      <PricingPage />
+    </div>
+  );
+}
+  
